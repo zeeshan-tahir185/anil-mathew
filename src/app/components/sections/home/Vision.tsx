e0ac3099bb1f4a9c68cb9bbbf4a45f0f6284507f
Zeeshan <mzeeshi187@gmail.com>
2025-11-30 03:09:28 +0500

    home page near to complete

--- src/app/components/sections/home/Vision.tsx
+++ src/app/components/sections/home/Vision.tsx
@@ -21,17 +21,17 @@ const Vision = () => {
                     <div className='flex flex-col lg:flex-row gap-3 w-full'>
                         <div className='w-full lg:w-[600px] xl:w-[59%] relative '>
                             <Image src="/images/home/vision1.png" width={780} height={680} alt='vision 1 image' className='w-full z-10' />
-                            <p className='text-xl lg:text-2xl text-[#F7F4F1] font-semibold font-inter z-20 absolute leading-[26px] xl:leading-[24px] bottom-0 p-[33px] lg:p-[61px] left-0 lg:left-[61px]'>I build tools that make work <br className='hidden lg:block' /> feel lighter. </p>
+                            <p className='text-xl lg:text-2xl text-[#F7F4F1] font-semibold font-inter z-20 absolute leading-[26px] xl:leading-[24px] bottom-0 p-[33px] lg:p-[61px] left-0 '>I build tools that make work <br className='hidden lg:block' /> feel lighter. </p>
                         </div>
                         <div className='w-full lg:w-[400px] xl:w-[39.8%] relative'>
                             <Image src="/images/home/vision2.png" width={524} height={680} alt='vision 1 image' className='w-full z-10 ' />
-                            <p className='text-xl lg:text-2xl text-[#F7F4F1] font-semibold font-inter z-20 absolute leading-[26px] xl:leading-[24px] bottom-0 p-[25px] lg:p-[61px] left-0 lg:left-[61px]'>Products that remove friction, <br className='hidden lg:block' /> protect attention, and make the <br className='hidden lg:block' /> next step obvious.</p>
+                            <p className='text-xl lg:text-2xl text-[#F7F4F1] font-semibold font-inter z-20 absolute leading-[26px] xl:leading-[24px] bottom-0 p-[25px] lg:p-[61px] left-0 '>Products that remove friction, protect attention, and make the  next step obvious.</p>
                         </div>
                     </div>
                     <div className='w-full relative'>
                         <Image src="/images/home/vision3.png" width={780} height={680} alt='vision 1 image' className='w-full z-10 hidden lg:block' />
                         <Image src="/images/home/vision3_mobiles.png" width={780} height={680} alt='vision 1 image' className='w-full z-10 lg:hidden' />
-                        <p className='text-xl lg:text-2xl text-[#F7F4F1] font-semibold font-inter z-20 absolute leading-[26px] xl:leading-[24px] bottom-0 p-[25px] lg:p-[61px] left-0 lg:left-[61px]'>Each venture starts with a simple question: what would this look like if <br className='hidden lg:block' /> the software disappeared into the background?</p>
+                        <p className='text-xl lg:text-2xl text-[#F7F4F1] font-semibold font-inter z-20 absolute leading-[26px] xl:leading-[24px] bottom-0 p-[25px] lg:p-[61px] left-0 '>Each venture starts with a simple question: what would this look like if <br className='hidden lg:block' /> the software disappeared into the background?</p>
                     </div>
                 </div>
             </div>
