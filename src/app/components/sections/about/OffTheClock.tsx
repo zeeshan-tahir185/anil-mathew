b53f7173214d6d15d44c8d33a16c03233d52933d
Zeeshan <mzeeshi187@gmail.com>
2025-12-12 17:53:05 +0500

    TM added and title and search paste functionality added

--- src/app/components/sections/about/OffTheClock.tsx
+++ src/app/components/sections/about/OffTheClock.tsx
@@ -9,9 +9,9 @@ const OffTheClock = () => {
                     <h2 className='font-charter text-[36px] lg:text-[64px] leading-[36px] lg:leading-[64px] text-[#E8DCCB]' style={{ letterSpacing: "-2px" }}>Off the clock</h2>
                     <p className='text-[#F7F4F1] opacity-65 font-inter text-base lg:text-xl'>I chase big fish, fly light aircraft, and love fast cars that roar. The sea teaches patience. The sky teaches precision. Speed reminds me that power is nothing without control.</p>
                 </div>
-                <div className='flex flex-col xl:flex-row w-full gap-4 lg:gap-6 items-center justify-center'>
-                    <Image src="/images/about/clock1-new.png" width={652} height={550} alt='clock image' />
-                    <Image src="/images/about/clock2-new.png" width={652} height={550} alt='clock image' />
+                <div className='flex flex-col lg:flex-row w-full gap-4 lg:gap-6 items-center justify-center'>
+                    <Image src="/images/about/clock1-new.png" width={1402} height={1074} alt='clock image' className='w-full lg:w-[50%] h-full rounded-2xl object-cover' />
+                    <Image src="/images/about/clock2-new.png" width={1401} height={1074} alt='clock image' className='w-full lg:w-[50%] h-full rounded-2xl object-cover' />
                 </div>
             </div>
         </div>
